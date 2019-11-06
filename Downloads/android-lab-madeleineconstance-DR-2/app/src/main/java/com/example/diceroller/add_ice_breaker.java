dad1d7fd4bc1fdc2ca499c57e43724adaa2050a8
madeleineconstance <1807978@brunel.ac.uk>
2019-11-06 13:30:22 +0000

    android-lab-madeleineconstance-DR

---
+++ Downloads/android-lab-madeleineconstance-DR-2/app/src/main/java/com/example/diceroller/add_ice_breaker.java
@@ -0,0 +1,15 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_ice_breaker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ice_breaker);
+    }
+}
+
